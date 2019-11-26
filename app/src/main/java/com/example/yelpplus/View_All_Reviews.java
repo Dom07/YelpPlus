878ff9927dce274694cb5149a4e9fc9d45e8a067
ShreyasBhat1998 <shreyasbhat.bhat@gmail.com>
2019-11-26 16:18:25 -0500

    added graph

---
+++ app/src/main/java/com/example/yelpplus/View_All_Reviews.java
@@ -0,0 +1,13 @@
+package com.example.yelpplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_All_Reviews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
