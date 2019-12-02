6fb946118360a67f11ff7fbd7efd25d869e5fc51
Sagar Babariya <sagar.babariya77@gmail.com>
2019-12-02 18:53:05 -0500

    Created Event Booking Fragment and Event Booking class

---
+++ app/src/main/java/com/example/yelpplus/EventBooking.java
@@ -0,0 +1,52 @@
+package com.example.yelpplus;
+
+import com.google.gson.annotations.SerializedName;
+
+public class EventBooking {
+
+    @SerializedName("date")
+    private String date;
+
+    @SerializedName("time")
+    private String time;
+
+    @SerializedName("guest_count")
+    private  String guestCount;
+
+    @SerializedName("menu")
+    private String[] menu;
+
+
+    //Getter and setters
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getGuestCount() {
+        return guestCount;
+    }
+
+    public void setGuestCount(String guestCount) {
+        this.guestCount = guestCount;
+    }
+
+    public String[] getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String[] menu) {
+        this.menu = menu;
+    }
+}
